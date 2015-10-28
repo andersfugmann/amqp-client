b197b0adc79dccc193e2f8b08964f0bbcc311942
Anders Fugmann <anders@fugmann.net>
2015-10-28 18:06:49 +0100

    Use better types for message

--- src/amqp_message.ml
+++ src/amqp_message.ml
@@ -11,20 +11,25 @@ type t =
   }
 
 let make
-    ?content_type
-    ?content_encoding
-    ?headers
-    ?delivery_mode
-    ?priority
-    ?correlation_id
-    ?reply_to
-    ?expiration
-    ?message_id
-    ?timestamp
-    ?amqp_type
-    ?user_id
-    ?app_id
-    ?reserved body : message =
+    ?(content_type:string option)
+    ?(content_encoding: string option)
+    ?(headers: Amqp_types.table option)
+    ?(delivery_mode: int option)
+    ?(priority: int option)
+    ?(correlation_id: string option)
+    ?(reply_to: string option)
+    ?(expiration: int option)
+    ?(message_id: string option)
+    ?(timestamp: int option)
+    ?(amqp_type: string option)
+    ?(user_id: string option)
+    ?(app_id: string option)
+    body : message =
+  let expiration = match expiration with
+    | None -> None
+    | Some n -> Some (string_of_int n)
+  in
+
   ({ Content.content_type;
      content_encoding;
      headers;
@@ -38,5 +43,5 @@ let make
      amqp_type;
      user_id;
      app_id;
-     reserved;
+     reserved = None;
    }, body)
