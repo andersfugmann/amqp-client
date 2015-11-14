a795e3c02982b60a974d7df2a1ed6123532fe496
Anders Fugmann <anders@fugmann.net>
2015-11-14 14:58:59 +0100

    Add test of close

---
+++ tests/close_test.ml
@@ -0,0 +1,39 @@
+open Async.Std
+open Amqp
+
+let log fmt = printf (fmt ^^ "\n%!")
+
+let handler var { Message.message = (_, body); _ } = Ivar.fill var body; return ()
+
+let test () =
+  Connection.connect ~virtual_host:"/test" ~id:"ocaml-amqp-test" "localhost" >>= fun connection ->
+  log "Connection started";
+  Connection.open_channel ~id:"queue.test" Channel.no_confirm connection >>= fun channel ->
+  log "Channel opened";
+  Queue.declare channel ~auto_delete:false "queue.test" >>= fun queue ->
+  log "Queue declared";
+  Queue.publish channel queue (Message.make "Test") >>= fun res ->
+  assert (res = `Ok);
+  log "Message published";
+  Connection.close connection >>= fun () ->
+  log "Connection closed";
+  Connection.connect ~virtual_host:"/test" ~id:"ocaml-amqp-test" "localhost" >>= fun connection ->
+  log "Connection started";
+  Connection.open_channel ~id:"queue.test" Channel.no_confirm connection >>= fun channel ->
+  log "Channel opened";
+  Queue.declare channel ~auto_delete:false "queue.test" >>= fun queue ->
+  log "Queue declared";
+  Queue.get ~no_ack:false channel queue >>= fun m ->
+  (match m with
+    | None -> failwith "No message"
+    | Some _ -> ()
+  );
+  log "Message received";
+  Queue.delete channel queue >>= fun () ->
+  log "Queue deleted";
+  Connection.close connection >>| fun () ->
+  Shutdown.shutdown 0
+
+let _ =
+  test () |> ignore;
+  Scheduler.go ()
