1019412e997031aa3b99bd0da1f6b119a3e1bb3e
Anders Fugmann <anders@fugmann.net>
2016-12-12 22:56:02 +0100

    Create general signature for async

--- src/amqp_thread_async.ml
+++ src/amqp_thread_async.ml
@@ -27,20 +27,11 @@ module Ivar = struct
   let create = Ivar.create
   let create_full = Ivar.create_full
   let fill = Ivar.fill
-  let read = Ivar.read
+  let read t = Ivar.read t
   let is_full = Ivar.is_full
   let fill_if_empty = Ivar.fill_if_empty
 end
 
-module Tcp = struct
-  let connect host port =
-    let addr = Tcp.to_host_and_port host port in
-    Tcp.connect ~buffer_age_limit:`Unlimited addr
-
-  let nodelay socket value =
-    Socket.setopt socket Socket.Opt.nodelay value
-
-end
 module Reader = struct
   type t = Reader.t
   let close = Reader.close
@@ -54,6 +45,19 @@ module Writer = struct
   let flush = Writer.flushed
 end
 
+module Tcp = struct
+  (* TODO. Actually set the nodelay *)
+  type socket = unit
+  let connect host port =
+    let addr = Tcp.to_host_and_port host port in
+    Tcp.connect ~buffer_age_limit:`Unlimited addr >>= fun (_s, r, w) -> return
+    ((), r, w)
+
+  let nodelay () (_value : bool) = ()
+  (* Socket.setopt socket Socket.Opt.nodelay value *)
+
+end
+
 module Log = struct
   (* Use of a predefiend tag allows the caller to disable logging if needed *)
   let tags = ["library", "amqp_client"]
@@ -64,22 +68,21 @@ end
 
 (* Pipes *)
 module Pipe = struct
-  type ('a, 'phantom) t = ('a, 'phantom) Pipe.t
-  let create = Pipe.create
-  let set_size_budget = Pipe.set_size_budget
-  let flush t = Pipe.downstream_flushed t
-  let interleave_pipe = Pipe.interleave_pipe
-  let write = Pipe.write
-  let write_without_pushback = Pipe.write_without_pushback
+  let create () = Pipe.create ()
+  let set_size_budget t = Pipe.set_size_budget t
+  let flush t = Pipe.downstream_flushed t >>= fun _ -> return ()
+  let interleave_pipe t = Pipe.interleave_pipe t
+  let write r elm = Pipe.write r elm
+  let write_without_pushback r elm = Pipe.write_without_pushback r elm
 
   let transfer_in ~from t =
     Queue.iter (write_without_pushback t) from;
     return ()
 
   let close t = Pipe.close t; flush t >>= fun _ -> return ()
-  let read = Pipe.read
-  let iter = Pipe.iter
-  let iter_without_pushback = Pipe.iter_without_pushback
+  let read r = Pipe.read r
+  let iter r ~f = Pipe.iter r ~f
+  let iter_without_pushback r ~f = Pipe.iter_without_pushback r ~f
 
   module Writer = struct
     type 'a t = 'a Pipe.Writer.t
@@ -91,6 +94,6 @@ module Pipe = struct
 end
 
 module Scheduler = struct
-  let go = Scheduler.go
+  let go () = Scheduler.go () |> ignore
   let shutdown n = Shutdown.shutdown n
 end
