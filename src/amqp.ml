47b602d9386f0acb10d82f882e0755ccc4bb0756
Anders Fugmann <anders@fugmann.net>
2016-12-14 23:55:55 +0100

    WIP

--- src/amqp.ml
+++ src/amqp.ml
@@ -2,11 +2,11 @@
     This module contains convenient module aliases
 *)
 module Make(Thread : Amqp_thread.T) = struct
-  module Thread = Thread
   module Message = Amqp_message.Make(Thread)
   module Connection = Amqp_connection.Make(Thread)
   module Queue = Amqp_queue.Make(Thread)
   module Channel = Amqp_channel.Make(Thread)
   module Exchange = Amqp_exchange.Make(Thread)
   module Rpc = Amqp_rpc.Make(Thread)
+  module Thread = Thread
 end
