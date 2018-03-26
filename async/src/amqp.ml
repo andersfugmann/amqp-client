20e4263293790c4bfe952abeebf3b9d441ad55ce
Anders Fugmann <anders@fugmann.net>
2018-03-26 12:19:25 +0200

    Remove internal modules from Amqp module

--- async/src/amqp.ml
+++ async/src/amqp.ml
@@ -3,12 +3,7 @@ open  Amqp_client_lib
 module Channel = Channel
 module Connection = Connection
 module Exchange = Exchange
-module Framing = Framing
-module Io = Io
 module Message = Message
-module Protocol_helpers = Protocol_helpers
-module Protocol = Protocol
 module Queue = Queue
 module Rpc = Rpc
 module Types = Types
-module Thread = Thread
