11b3c96a591836ccb436e8df8ac6c21084198926
Anders Fugmann <anders@fugmann.net>
2018-11-22 10:27:29 +0100

    Make types non-opaque for async version to allow using Async function on eg. pipes

--- async/src/thread.mli
+++ async/src/thread.mli
@@ -1,1 +1,7 @@
-include Amqp_client_lib.Thread.T with type 'a Deferred.t = 'a Async_kernel.Deferred.t
+include Amqp_client_lib.Thread.T
+  with type 'a Deferred.t = 'a Async_kernel.Deferred.t
+  and type Writer.t = Async.Writer.t
+  and type Reader.t = Async.Reader.t
+  and type 'a Pipe.Reader.t = 'a Async.Pipe.Reader.t
+  and type 'a Pipe.Writer.t = 'a Async.Pipe.Writer.t
+  and type 'a Ivar.t = 'a Async_kernel.Ivar.t
