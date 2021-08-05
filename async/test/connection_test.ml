8f4ff3ccad411201c0639d38ce2763d05cea2cac
Anders Fugmann <anders@fugmann.net>
2021-08-05 22:21:32 +0200

    Make sure all tests uses AMQP_PORT if set

--- async/test/connection_test.ml
+++ async/test/connection_test.ml
@@ -5,8 +5,9 @@ let uniq s =
   Printf.sprintf "%s_%d_%s" (Filename.basename Sys.argv.(0)) (Unix.getpid ()) s
 
 let test =
-  Connection.connect ~id:(uniq "ocaml-amqp-tests") "localhost" >>= fun connection1 ->
-  Connection.connect ~id:(uniq "ocaml-amqp-tests1") "localhost" >>= fun connection2 ->
+  let port = Sys.getenv_opt "AMQP_PORT" |> Option.map int_of_string in
+  Connection.connect ?port ~id:(uniq "ocaml-amqp-tests") "localhost" >>= fun connection1 ->
+  Connection.connect ?port ~id:(uniq "ocaml-amqp-tests1") "localhost" >>= fun connection2 ->
   Connection.close connection1 >>= fun () ->
   Connection.close connection2 >>| fun () ->
   Log.info "Connections closed";
