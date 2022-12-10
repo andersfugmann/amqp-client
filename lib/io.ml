7e2b9925e4f7342feca3f2ae752f680beeb39451
Anders Fugmann <anders@fugmann.net>
2022-12-10 14:49:55 +0100

    Remove dependency on ocplib-endian

--- lib/io.ml
+++ lib/io.ml
@@ -1,8 +1,7 @@
 (** Internal *)
 
 module Input = struct
-  open EndianBytes.BigEndian
-  type t = { buf: Bytes.t; mutable offset: int }
+  type t = { buf: String.t; mutable offset: int }
   let init ?(offset=0) buf = { buf; offset }
   let read f n t =
     let r = f t.buf t.offset in
@@ -10,31 +9,27 @@ module Input = struct
     r
 
   let string t len =
-    let s =
-      Bytes.sub t.buf t.offset len
-      |> Bytes.to_string
-    in
+    let s = String.sub t.buf t.offset len in
     t.offset <- t.offset + len;
     s
 
-  let octet = read get_uint8 1
-  let short = read get_uint16 2
-  let long t = read get_int32 4 t |> Int32.to_int
-  let longlong t = read get_int64 8 t |> Int64.to_int
-  let float = read get_float 4
-  let double = read get_double 8
+  let octet = read String.get_uint8 1
+  let short = read String.get_uint16_be 2
+  let long t = read String.get_int32_be 4 t |> Int32.to_int
+  let longlong t = read String.get_int64_be 8 t |> Int64.to_int
+  let float = read (fun s l -> String.get_int32_be s l |> Int32.float_of_bits) 2
+  let double = read (fun s l -> String.get_int64_be s l |> Int64.float_of_bits) 8
 
-  let length t = Bytes.length t.buf - t.offset
+  let length t = String.length t.buf - t.offset
   let has_data t = length t > 0
   let offset t = t.offset
 
   let copy t ~dst_pos ~len (dst:Bytes.t) =
-    Bytes.blit t.buf t.offset dst dst_pos len;
+    Bytes.blit_string t.buf t.offset dst dst_pos len;
     t.offset <- t.offset + len
 end
 
 module Output = struct
-  open EndianBytes.BigEndian
   type t = { mutable buf: Bytes.t; mutable offset: int; apply: bool }
   let create len = { buf = Bytes.create len; offset = 0; apply = true }
 
@@ -54,23 +49,23 @@ module Output = struct
       Bytes.blit_string src src_pos t.buf t.offset len;
     t.offset <- t.offset + len
 
-  let octet = write set_int8 1
-  let short = write set_int16 2
+  let octet = write Bytes.set_int8 1
+  let short = write Bytes.set_int16_be 2
   let short_ref t =
     let offset = t.offset in
     t.offset <- t.offset + 2;
     fun v ->
-      if (t.apply) then set_int16 t.buf offset v
+      if (t.apply) then Bytes.set_int16_be t.buf offset v
 
-  let long = write (fun t pos v -> set_int32 t pos (Int32.of_int v)) 4
-  let longlong = write (fun t pos v -> set_int64 t pos (Int64.of_int v)) 8
-  let float = write set_float 4
-  let double = write set_double 8
+  let long = write (fun t pos v -> Bytes.set_int32_be t pos (Int32.of_int v)) 4
+  let longlong = write (fun t pos v -> Bytes.set_int64_be t pos (Int64.of_int v)) 8
+  let float = write (fun t pos v -> Bytes.set_int32_be t pos (Int32.bits_of_float v)) 4
+  let double = write (fun t pos v -> Bytes.set_int64_be t pos (Int64.bits_of_float v)) 8
   let size_ref t =
     let offset = t.offset in
     t.offset <- offset + 4;
     fun () ->
-      if t.apply then set_int32 t.buf offset (Int32.of_int (t.offset - (offset + 4)))
+      if t.apply then Bytes.set_int32_be t.buf offset (Int32.of_int (t.offset - (offset + 4)))
 
   let size t = t.offset
 end
